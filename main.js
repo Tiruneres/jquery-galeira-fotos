0bf33f66d361f4731a3071a9e0f778eac0c65e9a
Kleber Neres <nereskleber95@gmail.com>
2023-11-23 16:02:49 -0300

    commit inicial

--- main.js
+++ main.js
@@ -1,32 +1,25 @@
 $(document).ready(function() {
 
-    $('header button').click(function() {
-
-    $('form').slideDown();
-
-    })
-
-    $('#botao-cancelar').click(function(){
-
-        $('form').slideUp();
-
-    })
-
-    $('form').on('submit', function(e) {
+    
+    $('form').on('submit', function(e){
+        
         e.preventDefault();
-        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
-        const novoItem = $('<li style="display:none" ></li>');
-        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
-        $(`
-            <div class="overlay-imagem-link">
-                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
-                    Ver imagem em tamanho real
-                </a>
-            </div>
-            `).appendTo(novoItem);
-            $(novoItem).appendTo('ul');
-            $(novoItem).fadeIn(1000);
-            $('#endereco-imagem-nova').val('');
+
+        const tarefa = $('#inputs').val();
+
+        const novaTarefa = $('<li></li>');
+
+        
+        $(`<p>${tarefa}</p>`).appendTo(novaTarefa);
+        $(novaTarefa).appendTo('ul');
+        
+        $('p').click(function(){
+            $(`<p class="aprovado"></p>`);
+        })
+        
     })
+    
+
 
 })
+
